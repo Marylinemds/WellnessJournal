49671c2e6ab4962e5d2d45db8fb025a7cbebf78f
Marylinemds <mendes.escom@gmail.com>
2017-09-17 16:11:11 +0200

    food expandable list view aligned and filled with data

--- app/src/main/java/com/example/android/wellnessjournal/Food/FoodActivity.java
+++ app/src/main/java/com/example/android/wellnessjournal/Food/FoodActivity.java
@@ -8,11 +8,17 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
 import android.widget.ToggleButton;
 
+import com.example.android.wellnessjournal.ExpandableListAdapter;
 import com.example.android.wellnessjournal.R;
 import com.example.android.wellnessjournal.Utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Maryline on 9/16/2017.
@@ -24,11 +30,21 @@ public class FoodActivity extends AppCompatActivity{
     private Context mContext = FoodActivity.this;
     private static final int ACTIVITY_NUM = 1;
 
+    private ExpandableListView listView;
+    private ExpandableListAdapter listAdapter;
+    private List<String> listDataHeader;
+    private HashMap<String,List<String>> listHash;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
         Log.d(TAG, "onCreate: started.");
+
+        listView = (ExpandableListView)findViewById(R.id.expandableListView);
+        initData();
+        listAdapter = new ExpandableListAdapter(this,listDataHeader,listHash);
+        listView.setAdapter(listAdapter);
 
         setupBottomNavigationView();
 
@@ -45,8 +61,39 @@ public class FoodActivity extends AppCompatActivity{
         menuItem.setChecked(true);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+    private void initData() {
+        listDataHeader = new ArrayList<>();
+        listHash = new HashMap<>();
+
+        listDataHeader.add("Water intake");
+        listDataHeader.add("Intermittent Fasting");
+        listDataHeader.add("Special treats");
+        listDataHeader.add("General comment");
+
+        List<String> waterIntake = new ArrayList<>();
+        waterIntake.add("How much water did you have today (in mL)?");
+
+        List<String> intermittentFasting = new ArrayList<>();
+        intermittentFasting.add("How many hours between last dinner and breakfast ?");
+
+
+        List<String> specialTreats = new ArrayList<>();
+        specialTreats.add("Processed food");
+        specialTreats.add("Meat/Fish");
+        specialTreats.add("Egg");
+        specialTreats.add("Fried/Oil");
+        specialTreats.add("white sugar");
+
+        specialTreats.add("white flour");
+
+
+        List<String> generalComment = new ArrayList<>();
+        generalComment.add("Write your comment here");
+
+
+        listHash.put(listDataHeader.get(0),waterIntake);
+        listHash.put(listDataHeader.get(1),intermittentFasting);
+        listHash.put(listDataHeader.get(2),specialTreats);
+        listHash.put(listDataHeader.get(3),generalComment);
     }
 }
